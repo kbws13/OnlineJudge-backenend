76901b7f58d30e029fcdee1ada53f8292c22bb2d
kbws <hsy040506@163.com>
2023-10-08 19:43:06 +0800

    init

---
+++ src/main/java/xyz/kbws/OjBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.kbws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/kbws/OjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.kbws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
