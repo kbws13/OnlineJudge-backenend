2c5d62e43a4492d898b42c4bf4a670fdba40bb20
kbws <hsy040506@163.com>
2023-11-11 18:01:08 +0800

    测试功能并修改完善

--- src/main/java/xyz/kbws/judge/codesandbox/impl/RemoteCodeSandBox.java
+++ src/main/java/xyz/kbws/judge/codesandbox/impl/RemoteCodeSandBox.java
@@ -1,5 +1,10 @@
 package xyz.kbws.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import org.apache.commons.lang3.StringUtils;
+import xyz.kbws.common.ErrorCode;
+import xyz.kbws.exception.BusinessException;
 import xyz.kbws.judge.codesandbox.CodeSandBox;
 import xyz.kbws.judge.codesandbox.model.ExecuteCodeRequest;
 import xyz.kbws.judge.codesandbox.model.ExecuteCodeResponse;
@@ -10,9 +15,25 @@ import xyz.kbws.judge.codesandbox.model.ExecuteCodeResponse;
  * @description: 远程代码沙箱
  */
 public class RemoteCodeSandBox implements CodeSandBox {
+
+    // 定义鉴权请求头和密钥
+    public static final String AUTH_REQUEST_HEADER = "auth";
+
+    public static final String AUTH_REQUEST_SECRET = "secret";
+
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                //.header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
+                .body(json)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 }
--- src/test/java/xyz/kbws/judge/codesandbox/CodeSandBoxTest.java
+++ src/test/java/xyz/kbws/judge/codesandbox/CodeSandBoxTest.java
@@ -66,7 +66,13 @@ class CodeSandBoxTest {
     void executeCodeByProxy() {
         CodeSandBox codeSandBox = CodeSandBoxFactory.newInstance(type);
         codeSandBox = new CodeSandBoxProxy(codeSandBox);
-        String code = "int main(){}";
+        String code = "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int a = Integer.parseInt(args[0]);\n" +
+                "        int b = Integer.parseInt(args[1]);\n" +
+                "        System.out.println(\"结果:\" + (a+b));\n" +
+                "    }\n" +
+                "}";
         String language = QuestionSubmitLanguageEnum.JAVA.getValue();
         List<String> inputList = Arrays.asList("1 2", "3 4");
         ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
@@ -75,7 +81,7 @@ class CodeSandBoxTest {
                 .inputList(inputList)
                 .build();
         ExecuteCodeResponse executeCodeResponse = codeSandBox.executeCode(executeCodeRequest);
-        //Assertions.assertNotNull(executeCodeResponse);
+        Assertions.assertNotNull(executeCodeResponse);
     }
 
     public static void main(String[] args) {
